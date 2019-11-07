cc752207002c78c8a9a9fadabeb1ba68565e58b7
nrrao <nandanarao@yahoo.com>
2019-11-06 22:50:08 -0500

    logging dates

--- src/habits/habits-service.js
+++ src/habits/habits-service.js
@@ -3,7 +3,7 @@ var moment = require('moment-timezone');
 const HabitsService = {
   getAllDatesForHabit(db,habitId){
     const past = moment.tz('America/New_York').subtract(4,'days').format('YYYY-MM-DD')
-    const now =  moment.tz('America/New_York')
+    const now =  moment.tz('America/New_York').format()
     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^ ", past, now)
     return db
     .from('habit_dates')
